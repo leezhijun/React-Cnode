78d510d4a253ed800651b4474594cbaf7f78b7bb
leezhijun <903226828@qq.com>
2018-12-20 14:14:17 +0800

    Updates

--- precache-manifest.9510c37fa4d69cf78024b04d0ed045bf.js
+++ precache-manifest.9510c37fa4d69cf78024b04d0ed045bf.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/React-Cnode/static/js/runtime~main.6b42319b.js"
   },
   {
-    "revision": "9f936dfa6d91a2ee3b67",
-    "url": "/React-Cnode/static/js/main.9f936dfa.chunk.js"
+    "revision": "c0ce3f80f51494cc5e0d",
+    "url": "/React-Cnode/static/js/main.c0ce3f80.chunk.js"
   },
   {
     "revision": "bcaaa531bd68e7cc4021",
     "url": "/React-Cnode/static/js/1.bcaaa531.chunk.js"
   },
   {
-    "revision": "9f936dfa6d91a2ee3b67",
+    "revision": "c0ce3f80f51494cc5e0d",
     "url": "/React-Cnode/static/css/main.65f2ba03.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/React-Cnode/static/css/1.c2fb2ac7.chunk.css"
   },
   {
-    "revision": "c38a7fd163bec04f2bf5856423ae21c8",
+    "revision": "7df914fb72dacb027f7939400ad756bb",
     "url": "/React-Cnode/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Cnode/precache-manifest.7d66ca5e0cf3a5b4fcd5a7152d78575d.js"
+  "/React-Cnode/precache-manifest.9510c37fa4d69cf78024b04d0ed045bf.js"
 );
 
 workbox.clientsClaim();
